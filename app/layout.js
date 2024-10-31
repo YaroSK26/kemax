547fdca6e50e2642d0bbf6f33dc9b01cf73cad40
jaxo <jaroba0@gmail.com>
2024-10-31 17:25:29 +0100

    Initial commit from Create Next App

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import "./globals.css";
+
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
