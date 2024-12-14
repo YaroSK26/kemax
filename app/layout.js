4c198e35d0ce6f1a1039caf87acab1d6f88a5b19
jaxo <jaroba0@gmail.com>
2024-12-14 13:13:24 +0100

    uprava metatad

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,30 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-
+export const metadata = {
+  title: "Kemax s.r.o.",
+  description:
+    "Kemax s.r.o. - náš výrobný sortiment zahŕňa hladké plechy, vlnité plechy a trapézové profily, klampiarské výrobky",
+  keywords: "Kemax s.r.o., Košice, hladké, vlnité a trapézové plechy",
+  authors: [{ name: "Jaroslav Barabáš" }],
+  openGraph: {
+    locale: "sk_SK",
+    type: "website",
+    url: "https://www.kemax.sk/",
+    title: "Úvod Kemax s.r.o.",
+    description:
+      "Kemax s.r.o. - náš výrobný sortiment zahŕňa hladké plechy, vlnité plechy a trapézové profily, klampiarské výrobky",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="sk">
+      
       <body>
-        <Navbar></Navbar>
+        <Navbar />
         {children}
-        <Footer ></Footer>
+        <Footer />
       </body>
     </html>
   );
