3d376b28668e64813ad51552e906e644597ad662
jaxo <jaroba0@gmail.com>
2025-01-02 18:10:23 +0100

    uprava redirectov

--- next.config.js
+++ next.config.js
@@ -2,6 +2,30 @@
 const nextConfig = {
   output: "export",
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/3-Plechy.html',
+        destination: 'https://www.kemax.sk/sk/3-Plechy.html',
+        permanent: true,
+      },
+      {
+        source: '/12-Sluzby.html',
+        destination: 'https://www.kemax.sk/sk/12-Sluzby.html',
+        permanent: true,
+      },
+      {
+        source: '/5-Technologie.html',
+        destination: 'https://www.kemax.sk/sk/5-Technologie.html',
+        permanent: true,
+      },
+      {
+        source: '/7-Kontakt.html',
+        destination: 'https://www.kemax.sk/sk/7-Kontakt.html',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
