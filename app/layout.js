680eeb732f3ac1014e8e73df435bf8fa6fbad0e0
jaxo <jaroba0@gmail.com>
2025-01-16 18:22:44 +0100

    analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import CookieConsent from "../components/CookieConsent";
+import { Analytics } from "@vercel/analytics/react";
   
 export const metadata = {
   title: "Kemax s.r.o.",
