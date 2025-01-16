2d7160e8afda26d59af7a0e928e191ff52d92573
jaxo <jaroba0@gmail.com>
2025-01-16 18:23:12 +0100

    :/

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="sk">
       <body>
         <Navbar />
+        <Analytics/>
         {children}
         <CookieConsent></CookieConsent>
         <Footer />
